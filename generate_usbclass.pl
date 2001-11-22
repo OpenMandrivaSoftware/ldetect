b63e07a46b8f89b975ed8bb12232cbf3ef01ed07
gc <gc@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2001-11-22 23:16:03 +0000

    - don't limitate to CLASS ID's with a Protocol number - fix bug that made ID's with a-f not appear (\d not very   good to parse  hexadecimal)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@41300 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- generate_usbclass.pl
+++ generate_usbclass.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-print q(/* This auto-generated from <usb.h>, don't modify! */
+print q(/* This is auto-generated from </usr/share/usb.ids>, don't modify! */
 
 struct {
   unsigned long id;
@@ -10,17 +10,24 @@ struct {
 
 while (<>) {
     chomp;
-    if (/^C\s+(\d+)\s+(.*)/) {
+    if (/^C\s+([\da-f]+)\s+(.*)/) {  #- I want alphanumeric but I can't get this [:alnum:] to work :-(
 	($cat, $cat_descr) = ($1, $2);
-    } elsif (/^\t(\d+)\s+(.*)/ && defined $cat) {
+    } elsif (/^\t([\da-f]+)\s+(.*)/ && defined $cat) {
 	($sub, $sub_descr) = ($1, $2);
-	$sub =~ /^\d\d$/ or die "bad line $.: sub category number badly formatted ($_)\n";
-    } elsif (/^\t\t(\d+)\s+(.*)/ && defined $cat) {
-	print qq(/* $. */  { 0x$cat$sub$1, "$cat_descr|$sub_descr|$2" },\n);
+	$sub =~ /^[\da-f]{2}$/ or die "bad line $.: sub category number badly formatted ($_)\n";
+	push @without_prot, [ "0x$cat$sub"."00", "/* $. */  { 0x$cat$sub".qq(00, "$cat_descr|$sub_descr" },\n) ];
+    } elsif (/^\t\t([\da-f]+)\s+(.*)/ && defined $cat) {
+	push @everything, qq(/* $. */  { 0x$cat$sub$1, "$cat_descr|$sub_descr|$2" },\n);
     } elsif (/^\S/) {
       undef $cat;
     }
 }
+
+foreach $l (@without_prot) {
+    grep { /{ $l->[0], / } @everything or push @everything, $l->[1];
+}
+
+print sort @everything;
 
 print '
 };
