9be7e13528c9020788281160842b31adb7ac2bb2
tvignaud <tvignaud@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2002-07-09 21:28:09 +0000

    fix for "gcc-2.95.3 don't compile" (reported by Ian White)
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@41329 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- lspcidrake.c
+++ lspcidrake.c
@@ -4,7 +4,6 @@
 
 static int verboze = 0;
 
-
 typedef const char *(f_t)(unsigned long );
 
 static void printit(struct pciusb_entries *entries, f_t find_class) {
@@ -33,6 +32,7 @@ static void printit(struct pciusb_entries *entries, f_t find_class) {
 int main(int argc, char **argv) {
 	char ** ptr = argv;
 	int full_probe = 0;
+	struct pciusb_entries entries;
 
 	while (ptr && *ptr) {
 		if (!strcmp(*ptr, "-h") || !strcmp(*ptr, "--help")) {
@@ -48,7 +48,7 @@ int main(int argc, char **argv) {
 		ptr++;
 	}
 
-	struct pciusb_entries entries = pci_probe(full_probe);
+	entries = pci_probe(full_probe);
 	printit(&entries, pci_class2text);
 	pciusb_free(&entries);
 
