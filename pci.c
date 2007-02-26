7b413284c8d5b9cc8e87f7c14c7fa358984e90fa
tv <tv@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2007-02-26 13:53:02 +0000

    (pci_probe) remove an extra space wronly added in r125837
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@44669 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- pci.c
+++ pci.c
@@ -60,7 +60,7 @@ extern struct pciusb_entries pci_probe(void) {
 
 		pciusb_initialize(e);
 
-          asprintf(&e->text, " %s|%s",
+          asprintf(&e->text, "%s|%s",
                    pci_lookup_name(pacc, vendorbuf, sizeof(vendorbuf), PCI_LOOKUP_VENDOR, dev->vendor_id, dev->device_id),
                    pci_lookup_name(pacc, devbuf,    sizeof(devbuf),    PCI_LOOKUP_DEVICE, dev->vendor_id, dev->device_id)
                );
