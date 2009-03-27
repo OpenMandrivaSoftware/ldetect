faf24618d6d1d6458e7d4b05292166a41ed0be40
blino <blino@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2009-03-27 18:41:39 +0000

    do not use random string as device description
    broken since commit r225445 from 2007-08-06: (pciusb_find_modules) handle pcitable without description field
    
    Broken output was the following:
    Mouse:USB|Generic 2 Button Mouse: base.frm"
     [Human Interface Device|Boot Interface Subclass|Mouse] (vendor:0603 device:6871)
    
    
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@254954 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- pciusb.c
+++ pciusb.c
@@ -205,7 +205,7 @@ extern int pciusb_find_modules(struct pciusb_entries *entries, const char *fpciu
 				e->module = strndup(p,q-p-1);
 			}
 			/* special case for buggy 0x0 usb entry */
-			if (descr_lookup == LOAD && 2 < strlen(q+2) && vendor != 0 && device != 0 && e->class_id != 0x90000d) { /* Hub class */
+			if (descr_lookup == LOAD && strlen(q) > 1 && 2 < strlen(q+2) && vendor != 0 && device != 0 && e->class_id != 0x90000d) { /* Hub class */
 				ifree(e->text); /* usb.c set it so that we display something when usbtable doesn't refer that hw*/
 				e->text = strndup(q+2, strlen(q)-4);
 			}
