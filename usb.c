b3a723ca7868cff83505c5a44ed3b04e3aea370a
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-10-30 13:20:51 +0000

    allocate string on stack
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@275434 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- usb.c
+++ usb.c
@@ -9,7 +9,7 @@
 #include "common.h"
 #include "names.h"
 
-static const char *proc_usb_path_default = "/sys/kernel/debug/usb/devices";
+static const char proc_usb_path_default[] = "/sys/kernel/debug/usb/devices";
 const char *proc_usb_path = NULL;
 
 static void build_text(struct pciusb_entry *e, char *vendor_text, char *product_text) {
