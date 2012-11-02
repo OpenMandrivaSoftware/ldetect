4de76cbad78f84d52e99958c577356bcad7f4ea6
peroyvind <peroyvind@99302b65-d5f7-0310-b3dd-f8cd6f4e3d94>
2012-11-02 17:16:17 +0000

    store names of pci classes in a hash map for faster lookups
    git-svn-id: svn+ssh://svn.mandriva.com/svn/soft/ldetect/trunk@275470 99302b65-d5f7-0310-b3dd-f8cd6f4e3d94

--- generate_pciclass.pl
+++ generate_pciclass.pl
@@ -2,28 +2,23 @@
 
 print q(/* This auto-generated from <pci.h>, don't modify! */
 
+#include <map>
+#include <cstdint>
 #include "libldetect.h"
 
 namespace ldetect {
-static struct {
-  unsigned short id;
-  const char *name;
-} classes[] = {
+
+static const std::map<uint16_t,const char*> pciClasses {
 );
 
-/^#define\s+PCI_CLASS_(\w+)\s+(0x\w{4})/ and print qq(  { $2, "$1" },\n) while <>;
+/^#define\s+PCI_CLASS_(\w+)\s+(0x\w{4})/ and print qq(    { $2, "$1" },\n) while <>;
 
-print '
-};
-
-static const int nb_classes = sizeof(classes) / sizeof(*classes);
+print '};
 
 #pragma GCC visibility push(default) 
-const char *pci_class2text(unsigned long class_id) {
-  for (int i = 0; i < nb_classes; i++)
-    if (classes[i].id == class_id) return classes[i].name;
-
-  return pci_class2text(0);
+const char *pci_class2text(uint16_t classId) {
+    std::map<uint16_t, const char*>::const_iterator it = pciClasses.find(classId);
+    return it == pciClasses.end() ? "NOT_DEFINED" : it->second;
 }
 #pragma GCC visibility pop
 
