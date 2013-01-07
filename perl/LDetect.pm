daa261d639eac066bdc3749eb7ea27c74cd92954
proyvind (Per Øyvind Karlsen) <peroyvind@mandriva.org>
2013-01-07 23:28:13 +0100

    add casing to perl module naming to fix dependency extraction

---
+++ perl/LDetect.pm
@@ -0,0 +1,14 @@
+package LDetect;
+
+use strict;
+use warnings;
+
+use XSLoader;
+
+our @ISA = qw(); # help perl_checker
+our $VERSION = '0.13.0';
+# perl_checker: EXPORT-ALL
+
+XSLoader::load('ldetect', $VERSION);
+
+1;
